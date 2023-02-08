ca9ef4def252e6b539feb779e1d6c47f05cf67ca
ShanthoshPrabhu <shanthoshprabhu30@gmail.com>
2023-02-09 00:02:48 +0530

    moment fix

--- pages/search.js
+++ pages/search.js
@@ -72,7 +72,7 @@ function Search() {
               <div className=' text-black text-xs flex justify-center space-y-3 items-center py-2 p-2 m-2 rounded-md shadow-md' key={index} >{value}</div>
              ))}
             </div>
-             <div className=' text-[#37352fa6] text-xs font-semibold'>Posted {formattedDate}</div>
+             <div className=' text-[#37352fa6] text-xs font-semibold'>Posted <Moment fromNow>{post?.timestamp?.toDate()}</Moment></div>
           </div>
           
 })}
@@ -88,87 +88,3 @@ export default Search
 
 
 
-// const x = [
-//   {
-//     "id": "lH3F1bXLkHvj1knJNK8F",
-//     "category": [
-//         "Full Stack Developer",
-//         "Data Analyst"
-//     ],
-//     "data": [
-//         {
-//             "worktime": "fulltime",
-//             "pay": "Monthly-Rs 10,000",
-//             "role": "Full Stack Developer",
-//             "linknote": "",
-//             "note": "blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah ",
-//             "link": "",
-//             "noofposition": ""
-//         },
-//         {
-//             "linknote": "blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah ",
-//             "role": "Data Analyst",
-//             "noofposition": "2",
-//             "note": "blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah ",
-//             "worktime": "Intern",
-//             "pay": "12k per month",
-//             "link": "https://amazon.com"
-//         }
-//     ],
-//     "about": "This internship is about blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah ",
-//     "companyName": "Microsoft",
-//     "aboutCompany": "",
-//     "companyLink": "https://microsoft.com",
-//     "endNote": "",
-//     "timestamp": null,
-//     "postedBy": "Shanthosh Prabhu"
-// },
-// {
-//   "id": "i0EMvwUyIrdHR0iqiSpS",
-//   "endNote": "blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah ",
-//   "about": "This internship is about blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah ",
-//   "timestamp": null,
-//   "companyName": "Microsoft",
-//   "category": [
-//       "Full Stack Developer",
-//       "Data Analyst"
-//   ],
-//   "data": [
-//       {
-//           "worktime": "fulltime",
-//           "note": "blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah ",
-//           "noofposition": "10",
-//           "linknote": "blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah ",
-//           "link": "https://google.com",
-//           "role": "Full Stack Developer",
-//           "pay": "Monthly-Rs 10,000"
-//       },
-//       {
-//           "pay": "12k per month",
-//           "role": "Data Analyst",
-//           "noofposition": "2",
-//           "link": "https://amazon.com",
-//           "note": "blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah ",
-//           "worktime": "Intern",
-//           "linknote": "blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah "
-//       }
-//   ],
-//   "companyLink": "https://microsoft.com",
-//   "aboutCompany": "blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah ",
-//   "postedBy": "Shanthosh Prabhu"
-// }
-// ]
-
-// const currentDate = new Date();
-          // const timestamp = new Date(post.timestamp);
-          // const tenDaysInMilliseconds = 10 * 24 * 60 * 60 * 1000;
-
-          // let formattedDate;
-          // if (currentDate - timestamp >= tenDaysInMilliseconds) {
-          //   formattedDate = moment(timestamp).format("MMM YYYY");
-          // } else {
-          //   formattedDate = moment(timestamp).fromNow();
-          // }
-          // return (
-            
-          // )
